d5622e4315244617c2f0eba120a75b60ddc7c9ec
Hugo Vila <git@hugovila.nom.es>
2015-07-02 18:52:40 +0200

    Add little refactor

--- src/github-users-app.js
+++ src/github-users-app.js
@@ -1,9 +1,11 @@
 
     var xmlHttp = null;
     var baseUrl = 'https://api.github.com/search/users?q=';
+    var USERSTOSHOW = 10;
 
     function showSeachResult(info) {
         this.info = info;
+        var usersToShow = USERSTOSHOW;
         var searchResult = document.getElementById("search-result");
         var resultUserInfo = document.getElementById("result-user-info");
 
@@ -12,25 +14,26 @@
         // Crear nodo de tipo Element
         var newResultUserInfo = document.createElement("div");
         newResultUserInfo.setAttribute('id', 'result-user-info');
-        newResultUserInfo.setAttribute('class', "col-md-12 user-info");
+        newResultUserInfo.setAttribute('class', "col-md-12 result-user-info");
         searchResult.appendChild(newResultUserInfo);
         // searchResult.replaceChild(resultUserInfo, newResultUserInfo);
-        for (var i=0; i < 5; i += 1) {
+        for (var i=0; i < usersToShow; i += 1) {
             var parrafo = document.createElement("p");
-            parrafo.setAttribute('class', 'rslt');
+            parrafo.setAttribute('class', 'user-result');
             var image = document.createElement("img");
             image.setAttribute('src', this.info.items[i].avatar_url);
             image.style.maxWidth = "64px";
 
             // Crear nodo de tipo Text
-            var contenido = document.createTextNode(this.info.items[i].login);
+            var userLogin = document.createTextNode(this.info.items[i].login);
 
             // Añadir el nodo Text como hijo del nodo Element
-            parrafo.appendChild(contenido);
+            parrafo.appendChild(userLogin);
 
             // Añadir el nodo Element como hijo de la pagina
+            parrafo.appendChild(image);
             newResultUserInfo.appendChild(parrafo);
-            newResultUserInfo.appendChild(image);
+
         }
     }
 
