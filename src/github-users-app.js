6516b3c7e73b0c6ad4dd3c7f56eead95e2ce7880
Hugo Vila <git@hugovila.nom.es>
2015-07-03 20:39:11 +0200

    Add some refactor

--- src/github-users-app.js
+++ src/github-users-app.js
@@ -1,74 +1,89 @@
+/*global document, console */
+var xmlHttp = null;
+var baseUrl = 'https://api.github.com/search/users?q=';
+var USERSTOSHOW = 10;
 
-    var xmlHttp = null;
-    var baseUrl = 'https://api.github.com/search/users?q=';
-    var USERSTOSHOW = 10;
+function showSeachResult(info) {
+    //'use strict';
+    this.info = info;
+    if (info.total_count > USERSTOSHOW) {
+        this.usersToShow = USERSTOSHOW;
+    } else {
+        this.usersToShow = info.total_count;
+    }
+    var searchResult = document.getElementById("search-result"),
+        resultUserInfo = document.getElementById("result-user-info"),
+        i;
 
-    function showSeachResult(info) {
-        this.info = info;
-        var usersToShow = USERSTOSHOW;
-        var searchResult = document.getElementById("search-result");
-        var resultUserInfo = document.getElementById("result-user-info");
+    searchResult.removeChild(resultUserInfo);
 
-        searchResult.removeChild(resultUserInfo);
+    // Crear nodo de tipo Element
+    var newResultUserInfo = document.createElement("div");
+    newResultUserInfo.setAttribute('id', 'result-user-info');
+    newResultUserInfo.setAttribute('class', "col-md-12 result-user-info");
+    searchResult.appendChild(newResultUserInfo);
+    // searchResult.replaceChild(resultUserInfo, newResultUserInfo);
 
-        // Crear nodo de tipo Element
-        var newResultUserInfo = document.createElement("div");
-        newResultUserInfo.setAttribute('id', 'result-user-info');
-        newResultUserInfo.setAttribute('class', "col-md-12 result-user-info");
-        searchResult.appendChild(newResultUserInfo);
-        // searchResult.replaceChild(resultUserInfo, newResultUserInfo);
-        for (var i=0; i < usersToShow; i += 1) {
-            var parrafo = document.createElement("p");
-            parrafo.setAttribute('class', 'user-result');
-            var image = document.createElement("img");
-            image.setAttribute('src', this.info.items[i].avatar_url);
-            image.style.maxWidth = "64px";
+    var ulElement = document.createElement("ul");
+    var ilElement = document.createElement("il");
+    ulElement.appendChild(ilElement);
+    newResultUserInfo.appendChild(ulElement);
+    for (i = 0; i < this.usersToShow; i += 1) {
+        var userResult = document.createElement("div");
+        userResult.setAttribute('class', 'user-result');
 
-            // Crear nodo de tipo Text
-            var userLogin = document.createTextNode(this.info.items[i].login);
+        var imgElement = document.createElement("img");
+        console.log(this.info);
+        imgElement.setAttribute('src', this.info.items[i].avatar_url);
 
-            // Añadir el nodo Text como hijo del nodo Element
-            parrafo.appendChild(userLogin);
+        var pElement = document.createElement("p");
 
-            // Añadir el nodo Element como hijo de la pagina
-            parrafo.appendChild(image);
-            newResultUserInfo.appendChild(parrafo);
+        // Crear nodo de tipo Text
+        console.log(this.info);
+        var userLogin = document.createTextNode(this.info.items[i].login);
 
+        // Añadir el nodo Text como hijo del nodo Element
+        pElement.appendChild(userLogin);
+
+        // Añadir el nodo Element como hijo del elemento padre
+        userResult.appendChild(imgElement);
+        userResult.appendChild(pElement);
+        ilElement.appendChild(userResult);
+    }
+}
+
+function eventInputSearch(event) {
+    console.log(event.target.value);
+    httpGet(baseUrl + event.target.value);
+}
+
+function httpGet(theUrl) {
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = ProcessRequest;
+    xmlHttp.open("GET", theUrl, true);
+    xmlHttp.send(null);
+}
+
+function ProcessRequest() {
+if ( xmlHttp.readyState == 4 && xmlHttp.status == 200 ) {
+    if (xmlHttp.responseText == "Not found") {
+        document.getElementById("items-found").value = "Not found";
+        document.getElementById("raw-result").value = "";
+    } else {
+        var info = eval ( "(" + xmlHttp.responseText + ")" );
+
+        showSeachResult(info);
+
+        document.getElementById("items-found").value = info.total_count;
+        document.getElementById("raw-result").value = xmlHttp.responseText;
         }
     }
+}
 
-    function eventInputSearch(event) {
-        console.log(event.target.value);
-        httpGet(baseUrl + event.target.value);
-    }
-
-    function httpGet(theUrl) {
-        xmlHttp = new XMLHttpRequest();
-        xmlHttp.onreadystatechange = ProcessRequest;
-        xmlHttp.open("GET", theUrl, true);
-        xmlHttp.send(null);
-    }
-
-    function ProcessRequest() {
-    if ( xmlHttp.readyState == 4 && xmlHttp.status == 200 ) {
-        if (xmlHttp.responseText == "Not found") {
-            document.getElementById("items-found").value = "Not found";
-            document.getElementById("raw-result").value = "";
-        } else {
-            var info = eval ( "(" + xmlHttp.responseText + ")" );
-
-            showSeachResult(info);
-
-            document.getElementById("items-found").value = info.total_count;
-            document.getElementById("raw-result").value = xmlHttp.responseText;
-            }
-        }
-    }
-
-    function initialize(name) {
-        userSearchOnChange = document.getElementById('github-user-onchange');
-        userSearchOnChange.addEventListener('change', eventInputSearch, false);
-        userSearchOnInput = document.getElementById('github-user-oninput');
-        userSearchOnInput.addEventListener('input', eventInputSearch, false);
-        console.log("Initialize: Ok");
-    }
+function initialize() {
+    userSearchOnChange = document.getElementById('github-user-onchange');
+    userSearchOnChange.addEventListener('change', eventInputSearch, false);
+    userSearchOnInput = document.getElementById('github-user-oninput');
+    userSearchOnInput.addEventListener('input', eventInputSearch, false);
+    console.log("Initialize: Ok");
+}
